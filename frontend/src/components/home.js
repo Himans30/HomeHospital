d1e3c41d92e209dd5d5eca1f4ea0cada4aa2640b
Himans30 <himanshuyadav.3003@gmail.com>
2021-05-21 11:58:56 +0530

    ..

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -29,7 +29,7 @@ const Home = () => {
                             <li><a className="nav-link scrollto active" href="#hero">Home</a></li>
                             <li><a className="nav-link scrollto" href="#about">About</a></li>
                             <li><a className="nav-link scrollto" href="#services">Services</a></li>
-                            <li><a className="nav-link scrollto" href="#departments">Departments</a></li>
+                            
 
                             <li className="dropdown"><a href="#"><span>Drop Down</span> <i className="bi bi-chevron-down"></i></a>
                                 <ul>
@@ -117,12 +117,10 @@ const Home = () => {
                 <section id="about" className="about">
                     <div className="container-fluid">
 
-                        <div className="row">
-                            <div className="col-xl-5 col-lg-6 video-box d-flex justify-content-center align-items-stretch position-relative">
+                        
 
-                            </div>
 
-                            <div className="col-xl-7 col-lg-6 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-5">
+                           <div>
 
                                 <h3>About us</h3>
                                 <p>HomeHospital delivers quality care with compassion. We bring world class medical care into our patients’ homes and aim to make primary healthcare not only more accessible, but also more affordable and accountable to our patients’ needs..</p>
@@ -145,7 +143,7 @@ const Home = () => {
                                     <p className="description">Explicabo est voluptatum asperiores consequatur magnam. Et veritatis odit. Sunt aut deserunt minus aut eligendi omnis</p>
                                 </div>
 
-                            </div>
+                           
                         </div>
 
                     </div>
@@ -198,7 +196,7 @@ const Home = () => {
 
                         <div className="section-title">
                             <h2>Services</h2>
-                            <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+                            {/* <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p> */}
                         </div>
 
                         <div className="row">
@@ -255,166 +253,7 @@ const Home = () => {
                     </div>
                 </section>
 
-                <section id="appointment" className="appointment section-bg">
-                    <div className="container">
-
-                        <div className="section-title">
-                            <h2>Make an Appointment</h2>
-                            <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
-                        </div>
-
-                        <form action="forms/appointment.php" method="post" role="form" className="php-email-form">
-                            <div className="row">
-                                <div className="col-md-4 form-group">
-                                    <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                                    <div className="validate"></div>
-                                </div>
-                                <div className="col-md-4 form-group mt-3 mt-md-0">
-                                    <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-                                    <div className="validate"></div>
-                                </div>
-                                <div className="col-md-4 form-group mt-3 mt-md-0">
-                                    <input type="tel" className="form-control" name="phone" id="phone" placeholder="Your Phone" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                                    <div className="validate"></div>
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="col-md-4 form-group mt-3">
-                                    <input type="datetime" name="date" className="form-control datepicker" id="date" placeholder="Appointment Date" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                                    <div className="validate"></div>
-                                </div>
-                                <div className="col-md-4 form-group mt-3">
-                                    <select name="department" id="department" className="form-select">
-                                        <option value="">Select Department</option>
-                                        <option value="Department 1">Department 1</option>
-                                        <option value="Department 2">Department 2</option>
-                                        <option value="Department 3">Department 3</option>
-                                    </select>
-                                    <div className="validate"></div>
-                                </div>
-                                <div className="col-md-4 form-group mt-3">
-                                    <select name="doctor" id="doctor" className="form-select">
-                                        <option value="">Select Doctor</option>
-                                        <option value="Doctor 1">Doctor 1</option>
-                                        <option value="Doctor 2">Doctor 2</option>
-                                        <option value="Doctor 3">Doctor 3</option>
-                                    </select>
-                                    <div className="validate"></div>
-                                </div>
-                            </div>
-
-                            <div className="form-group mt-3">
-                                <textarea className="form-control" name="message" rows="5" placeholder="Message (Optional)"></textarea>
-                                <div className="validate"></div>
-                            </div>
-                            <div className="mb-3">
-                                <div className="loading">Loading</div>
-                                <div className="error-message"></div>
-                                <div className="sent-message">Your appointment request has been sent successfully. Thank you!</div>
-                            </div>
-                            <div className="text-center"><button type="submit">Make an Appointment</button></div>
-                        </form>
-
-                    </div>
-                </section>
-
-                <section id="departments" className="departments">
-                    <div className="container">
-
-                        <div className="section-title">
-                            <h2>Departments</h2>
-                            <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
-                        </div>
-
-                        <div className="row">
-                            <div className="col-lg-3">
-                                <ul className="nav nav-tabs flex-column">
-                                    <li className="nav-item">
-                                        <a className="nav-link active show" data-bs-toggle="tab" href="#tab-1">Cardiology</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" data-bs-toggle="tab" href="#tab-2">Neurology</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" data-bs-toggle="tab" href="#tab-3">Hepatology</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" data-bs-toggle="tab" href="#tab-4">Pediatrics</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" data-bs-toggle="tab" href="#tab-5">Eye Care</a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div className="col-lg-9 mt-4 mt-lg-0">
-                                <div className="tab-content">
-                                    <div className="tab-pane active show" id="tab-1">
-                                        <div className="row">
-                                            <div className="col-lg-8 details order-2 order-lg-1">
-                                                <h3>Cardiology</h3>
-                                                <p className="fst-italic">Qui laudantium consequatur laborum sit qui ad sapiente dila parde sonata raqer a videna mareta paulona marka</p>
-                                                <p>Et nobis maiores eius. Voluptatibus ut enim blanditiis atque harum sint. Laborum eos ipsum ipsa odit magni. Incidunt hic ut molestiae aut qui. Est repellat minima eveniet eius et quis magni nihil. Consequatur dolorem quaerat quos qui similique accusamus nostrum rem vero</p>
-                                            </div>
-                                            <div className="col-lg-4 text-center order-1 order-lg-2">
-                                                <img src="assets/img/departments-1.jpg" alt="" className="img-fluid" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="tab-pane" id="tab-2">
-                                        <div className="row">
-                                            <div className="col-lg-8 details order-2 order-lg-1">
-                                                <h3>Et blanditiis nemo veritatis excepturi</h3>
-                                                <p className="fst-italic">Qui laudantium consequatur laborum sit qui ad sapiente dila parde sonata raqer a videna mareta paulona marka</p>
-                                                <p>Ea ipsum voluptatem consequatur quis est. Illum error ullam omnis quia et reiciendis sunt sunt est. Non aliquid repellendus itaque accusamus eius et velit ipsa voluptates. Optio nesciunt eaque beatae accusamus lerode pakto madirna desera vafle de nideran pal</p>
-                                            </div>
-                                            <div className="col-lg-4 text-center order-1 order-lg-2">
-                                                <img src="assets/img/departments-2.jpg" alt="" className="img-fluid" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="tab-pane" id="tab-3">
-                                        <div className="row">
-                                            <div className="col-lg-8 details order-2 order-lg-1">
-                                                <h3>Impedit facilis occaecati odio neque aperiam sit</h3>
-                                                <p className="fst-italic">Eos voluptatibus quo. Odio similique illum id quidem non enim fuga. Qui natus non sunt dicta dolor et. In asperiores velit quaerat perferendis aut</p>
-                                                <p>Iure officiis odit rerum. Harum sequi eum illum corrupti culpa veritatis quisquam. Neque necessitatibus illo rerum eum ut. Commodi ipsam minima molestiae sed laboriosam a iste odio. Earum odit nesciunt fugiat sit ullam. Soluta et harum voluptatem optio quae</p>
-                                            </div>
-                                            <div className="col-lg-4 text-center order-1 order-lg-2">
-                                                <img src="assets/img/departments-3.jpg" alt="" className="img-fluid" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="tab-pane" id="tab-4">
-                                        <div className="row">
-                                            <div className="col-lg-8 details order-2 order-lg-1">
-                                                <h3>Fuga dolores inventore laboriosam ut est accusamus laboriosam dolore</h3>
-                                                <p className="fst-italic">Totam aperiam accusamus. Repellat consequuntur iure voluptas iure porro quis delectus</p>
-                                                <p>Eaque consequuntur consequuntur libero expedita in voluptas. Nostrum ipsam necessitatibus aliquam fugiat debitis quis velit. Eum ex maxime error in consequatur corporis atque. Eligendi asperiores sed qui veritatis aperiam quia a laborum inventore</p>
-                                            </div>
-                                            <div className="col-lg-4 text-center order-1 order-lg-2">
-                                                <img src="assets/img/departments-4.jpg" alt="" className="img-fluid" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="tab-pane" id="tab-5">
-                                        <div className="row">
-                                            <div className="col-lg-8 details order-2 order-lg-1">
-                                                <h3>Est eveniet ipsam sindera pad rone matrelat sando reda</h3>
-                                                <p className="fst-italic">Omnis blanditiis saepe eos autem qui sunt debitis porro quia.</p>
-                                                <p>Exercitationem nostrum omnis. Ut reiciendis repudiandae minus. Omnis recusandae ut non quam ut quod eius qui. Ipsum quia odit vero atque qui quibusdam amet. Occaecati sed est sint aut vitae molestiae voluptate vel</p>
-                                            </div>
-                                            <div className="col-lg-4 text-center order-1 order-lg-2">
-                                                <img src="assets/img/departments-5.jpg" alt="" className="img-fluid" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </section>
-
+ 
 
                 <section id="faq" className="faq section-bg">
                     <div className="container">
