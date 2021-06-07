6a4e3043e08259936d4ebe5ead33938ef6005213
Himans30 <himanshuyadav.3003@gmail.com>
2021-06-07 09:34:35 +0530

    ..

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
 const api_config = {
     port: 5000,
     db_url: 'mongodb+srv://HBBD:BBD1234@cluster0.gkwv4.mongodb.net/homehospital?retryWrites=true&w=majority',
-    stripe_sk: "sk_test_4ypbMh4aR9gRNnUkQCwgOyCT00rSoAbXzZ",
+    stripe_sk: "pk_test_51InFnsSJ9UKCtISEpNJl4j7gCljjZRP0Tm95xENmtL5ETPpMYnxr1GnkwRyN5FHq3uTdRDTyN0hrTJNxfS6TbkVa003YOTi4RO",
     email: 'hmanshu273201@gmail.com',
     app_password: 'lcqfbskappyzidhy'
 
