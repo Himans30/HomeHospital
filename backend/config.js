b266adc1804a44288585a470b3be41c6147e6abe
Himans30 <himanshuyadav.3003@gmail.com>
2021-06-07 09:30:18 +0530

    ..

--- backend/config.js
+++ backend/config.js
@@ -2,8 +2,8 @@ const api_config = {
     port: 5000,
     db_url: 'mongodb+srv://HBBD:BBD1234@cluster0.gkwv4.mongodb.net/homehospital?retryWrites=true&w=majority',
     stripe_sk: "sk_test_4ypbMh4aR9gRNnUkQCwgOyCT00rSoAbXzZ",
-    email: 'triplem656@gmail.com',
-    app_password: 'alktybnfbcfmsnkl'
+    email: 'hmanshu273201@gmail.com',
+    app_password: 'lcqfbskappyzidhy'
 
 }
 
