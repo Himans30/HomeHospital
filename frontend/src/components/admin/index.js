4aa8f3b432f67ea3290f0d22e389e4b4cc42b4d7
Himans30 <himanshuyadav.3003@gmail.com>
2021-06-14 10:34:25 +0530

    //

--- frontend/src/components/admin/index.js
+++ frontend/src/components/admin/index.js
@@ -65,7 +65,7 @@ const Admin = () => {
         {
             name: "ManageUsers",
             icon: <PeopleIcon />,
-            link: "/admin/manageusers"
+            link: "/admin/manageuser"
         },
         {
             name: "AddEquipment",
