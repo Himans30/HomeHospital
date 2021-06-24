3066270a6abeaf1aff4ac904e408c1f54049fb40
Himans30 <himanshuyadav.3003@gmail.com>
2021-06-24 10:40:13 +0530

    ..

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
 const api_config = {
     port: 5000,
     db_url: 'mongodb+srv://HBBD:BBD1234@cluster0.gkwv4.mongodb.net/homehospital?retryWrites=true&w=majority',
-    stripe_sk: "pk_test_51InFnsSJ9UKCtISEpNJl4j7gCljjZRP0Tm95xENmtL5ETPpMYnxr1GnkwRyN5FHq3uTdRDTyN0hrTJNxfS6TbkVa003YOTi4RO",
+    stripe_sk: "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",
     email: 'hmanshu273201@gmail.com',
     app_password: 'lcqfbskappyzidhy'
 
