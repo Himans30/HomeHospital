6cc4b0ea57718a9f14fe79a3ba894ed8ee5e1adc
Himans30 <himanshuyadav.3003@gmail.com>
2021-06-26 11:51:01 +0530

    ..

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -317,100 +317,14 @@ Our sales staff can also process other payment types manually, including checks 
                     </div>
                 </section>
 
-                <section id="testimonials" className="testimonials">
-                    <div className="container">
-
-                        <div className="testimonials-slider swiper-container" data-aos="fade-up" data-aos-delay="100">
-                            <div className="swiper-wrapper">
-
-                                <div className="swiper-slide">
-                                    <div className="testimonial-wrap">
-                                        <div className="testimonial-item">
-                                            <img src="assets/img/testimonials/testimonials-1.jpg" className="testimonial-img" alt="" />
-                                            <h3>Saul Goodman</h3>
-                                            <h4>Ceo &amp; Founder</h4>
-                                            <p>
-                                                <i className="bx bxs-quote-alt-left quote-icon-left"></i>
-                        Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
-                        <i className="bx bxs-quote-alt-right quote-icon-right"></i>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div className="swiper-slide">
-                                    <div className="testimonial-wrap">
-                                        <div className="testimonial-item">
-                                            <img src="assets/img/testimonials/testimonials-2.jpg" className="testimonial-img" alt="" />
-                                            <h3>Sara Wilsson</h3>
-                                            <h4>Designer</h4>
-                                            <p>
-                                                <i className="bx bxs-quote-alt-left quote-icon-left"></i>
-                        Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.
-                        <i className="bx bxs-quote-alt-right quote-icon-right"></i>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div className="swiper-slide">
-                                    <div className="testimonial-wrap">
-                                        <div className="testimonial-item">
-                                            <img src="assets/img/testimonials/testimonials-3.jpg" className="testimonial-img" alt="" />
-                                            <h3>Jena Karlis</h3>
-                                            <h4>Store Owner</h4>
-                                            <p>
-                                                <i className="bx bxs-quote-alt-left quote-icon-left"></i>
-                        Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.
-                        <i className="bx bxs-quote-alt-right quote-icon-right"></i>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div className="swiper-slide">
-                                    <div className="testimonial-wrap">
-                                        <div className="testimonial-item">
-                                            <img src="assets/img/testimonials/testimonials-4.jpg" className="testimonial-img" alt="" />
-                                            <h3>Matt Brandon</h3>
-                                            <h4>Freelancer</h4>
-                                            <p>
-                                                <i className="bx bxs-quote-alt-left quote-icon-left"></i>
-                        Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.
-                        <i className="bx bxs-quote-alt-right quote-icon-right"></i>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div className="swiper-slide">
-                                    <div className="testimonial-wrap">
-                                        <div className="testimonial-item">
-                                            <img src="assets/img/testimonials/testimonials-5.jpg" className="testimonial-img" alt="" />
-                                            <h3>John Larson</h3>
-                                            <h4>Entrepreneur</h4>
-                                            <p>
-                                                <i className="bx bxs-quote-alt-left quote-icon-left"></i>
-                        Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
-                        <i className="bx bxs-quote-alt-right quote-icon-right"></i>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div className="swiper-pagination"></div>
-                        </div>
-
-                    </div>
-                </section>
+                
 
                 <section id="gallery" className="gallery">
                     <div className="container">
 
                         <div className="section-title">
                             <h2>Gallery</h2>
-                            <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+                       
                         </div>
                     </div>
 
@@ -491,7 +405,7 @@ Our sales staff can also process other payment types manually, including checks 
 
                         <div className="section-title">
                             <h2>Contact</h2>
-                            <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+                            <p>A Home Hospital is always looking to make things easier for you. Our aim is to provide our customers with the best  facilities, constant care, and reliable support. If you would like to get in touch with a nurses/wardboys,etc from a specific service, would like some specific information about the services we provide, or just have a question for us, please fill up the Form given below and we will get back to you..</p>
                         </div>
                     </div>
 
